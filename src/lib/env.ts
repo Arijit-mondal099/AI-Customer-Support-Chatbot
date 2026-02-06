e8e40252a83dfa9741175ea11e8c5e0193ec4dab
Arijit Mondal <arijitm717@gmail.com>
2026-02-06 17:57:15 +0530

    db connection added

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,4 +4,5 @@ export const ENV = {
   SCALEKIT_ENVIRONMENT_URL: process.env.SCALEKIT_ENVIRONMENT_URL! as string,
   SCALEKIT_CLIENT_ID: process.env.SCALEKIT_CLIENT_ID! as string,
   SCALEKIT_CLIENT_SECRET: process.env.SCALEKIT_CLIENT_SECRET! as string,
+  MONGODB_URI: process.env.MONGODB_URI! as string,
 };
